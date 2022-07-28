24b44a3902e0c8f4e8c54be6704b5376aafc0487
Denver <denvercowan@rocketmail.com>
2022-07-28 15:52:52 -0400

    worked on styling

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+//declare variables needed within the project
 const mainContainer = document.querySelector(".mainContainer");
-//creates the gridsquares for the page
-for (let i = 0; i < 255; i++) {
-  const gridSquare = document.createElement("div");
-  gridSquare.classList.add("square");
-  mainContainer.append(gridSquare);
+const resize = document.querySelector(".resize");
+const reset = document.querySelector(".reset");
+//creates the default grid-squares for the page
+function defaultGrid() {
+  for (let i = 0; i < 256; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("square");
+    mainContainer.appendChild(gridSquare);
+  }
 }
+//resizes the grid
+resize.addEventListener("click", function resizeGrid() {
+  let rows = prompt("enter a grid size of up to 100!");
+  if (rows > 100 || rows < 0) {
+    alert("please enter a number between 1 and 100");
+  } else {
+    for (let i = 0; i < rows; i++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.classList.add("square");
+      mainContainer.appendChild(gridSquare);
+    }
+  }
+});
+
+//resets the grid
+reset.addEventListener("click", function resetGrid() {
+  defaultGrid();
+});
+
+defaultGrid();
