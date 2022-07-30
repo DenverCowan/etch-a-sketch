9231bb342ab384df4bcb99de0841a5e06a0e92e2
Denver <denvercowan@rocketmail.com>
2022-07-30 17:34:40 -0400

    finished project

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # etch-a-sketch
 
-This project is a part of the foundations course on The Odin Project. It is currently a work in progress
+This project is part of the odin foundations course, It took quite a bit of extra research outside of what they teach you to be able to do this project but I think it turned out alright.
+
+## Things I learned from this project
+
+-GIT branching can be a real life saver if you need to refactor your code but do not want to mess up what you already have
+-How to generate random RGB colors
+-A little bit of CSS grid, and some additional CSS attributes
+
+## Future plans
+
+-I would like to add a reset button so that the user doesn't have to reload the page to clear their sketch once they want to start a new one
--- main.js
+++ main.js
@@ -1,48 +1,135 @@
-//declare variables needed within the project
-const mainContainer = document.querySelector(".mainContainer");
-const resize = document.querySelector(".resize");
-const reset = document.querySelector(".reset");
-const random = document.querySelector(".random");
+//create buttons and cache the DOM
+const container = document.querySelector(".container");
+const btnBlack = document.createElement("button");
+const btnGray = document.createElement("button");
+const btnRandom = document.createElement("button");
+const btnSize = document.createElement("button");
+const buttonsContainer = document.querySelector(".buttons");
 
-//creates the default grid-squares for the page
-function defaultGrid() {
-  for (let i = 0; i < 256; i++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.add("square");
-    mainContainer.appendChild(gridSquare);
+//function to make the page load with the black color pencil
+window.onload = () => {
+  const boxes = container.querySelectorAll(".box");
+  boxes.forEach((box) =>
+    box.addEventListener("mouseenter", () => {
+      box.style.background = "black";
+    })
+  );
+};
+
+//function to create the grid divs
+function createDivs(col, row) {
+  for (let i = 0; i < col * row; i++) {
+    const div = document.createElement("div");
+    //div.style.border = "1px solid black";
+    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+    container.appendChild(div).classList.add("box");
   }
 }
-//resizes the grid
-resize.addEventListener("click", function resizeGrid() {
-  while (mainContainer.firstChild) {
-    mainContainer.removeChild(mainContainer.firstChild);
-  }
-  let rows = prompt("enter a grid size of up to 100!");
-  for (let i = 0; i < rows * rows; i++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.add("square");
-    mainContainer.appendChild(gridSquare);
-  }
-  coloring();
-});
+createDivs(16, 16);
 
-//resets the grid
-reset.addEventListener("click", function resetGrid() {
-  while (mainContainer.firstChild) {
-    mainContainer.removeChild(mainContainer.firstChild);
-  }
-  defaultGrid();
-  coloring();
-});
-//function for coloring in the squares with the mouse.
-function coloring() {
-  const squares = document.querySelectorAll(".square");
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("mouseenter", function (e) {
-      e.target.style.cssText = "background-color: black;";
-    });
-  }
+//function for Gray colors button
+function grayColor() {
+  const boxes = document.querySelectorAll(".box");
+  btnGray.textContent = "Gray";
+  btnGray.addEventListener("click", () => {
+    boxes.forEach((box) =>
+      box.addEventListener("mouseenter", () => {
+        let Rnum = Math.floor(Math.random() * 255);
+        box.style.background = `rgb(${Rnum}, ${Rnum}, ${Rnum})`;
+      })
+    );
+  });
+  buttonsContainer.appendChild(btnGray);
+  btnGray.classList.add("btn");
+}
+grayColor();
+
+//function for black colors button
+function blackColor() {
+  const boxes = document.querySelectorAll(".box");
+  btnBlack.textContent = "Black";
+  btnBlack.addEventListener("click", () => {
+    boxes.forEach((box) =>
+      box.addEventListener("mouseenter", () => {
+        box.style.background = "black";
+      })
+    );
+  });
+  buttonsContainer.appendChild(btnBlack).classList.add("btn");
+}
+blackColor();
+
+//function for RGB colors
+function RandomColor() {
+  const boxes = document.querySelectorAll(".box");
+  btnRandom.textContent = "Random";
+  btnRandom.addEventListener("click", () => {
+    boxes.forEach((box) =>
+      box.addEventListener("mouseenter", () => {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        box.style.background = `rgb(${r}, ${g}, ${b})`;
+      })
+    );
+  });
+  buttonsContainer.appendChild(btnRandom).classList.add("btn");
+}
+RandomColor();
+
+//function to reset grid
+function reset() {
+  const boxes = container.querySelectorAll(".box");
+  boxes.forEach((box) => box.remove());
 }
 
-defaultGrid();
-coloring();
+//function to resize grid
+function resize() {
+  btnSize.textContent = "Grid Size";
+  btnSize.addEventListener("click", () => {
+    let user = prompt("what size do you want your grid to be?");
+    if (user === null || user < 1 || user > 100) {
+      reset();
+      createDivs(16, 16);
+      grayColor();
+      blackColor();
+      RandomColor();
+      buttonsContainer.appendChild(btnSize).classList.add("btn");
+    } else {
+      reset();
+      createDivs(user, user);
+      grayColor();
+      blackColor();
+      RandomColor();
+      buttonsContainer.appendChild(btnSize).classList.add("btn");
+    }
+  });
+  buttonsContainer.appendChild(btnSize).classList.add("btn");
+}
+resize();
+
+//function for background animation
+function backgroundAnimation() {
+  colors = ["red", "blue", "yellow", "green", "purple"];
+  const section = document.querySelector(".section");
+  const span = document.createElement("span");
+
+  let size = Math.random() * 25;
+
+  span.style.width = 10 + size + "px";
+  span.style.height = 10 + size + "px";
+
+  span.style.borderRadius = `50%`;
+  span.style.top = Math.random() * innerHeight + "px";
+  span.style.left = Math.random() * innerWidth + "px";
+
+  const backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  span.style.background = backgroundColor;
+  section.appendChild(span);
+
+  setTimeout(() => {
+    span.remove;
+  }, 5000);
+}
+setInterval(backgroundAnimation, 500);
